ed583a1e88a93a1623707ab77bfdbcaea801be54
gaba-teixeira <gaba.andrade@outlook.com>
2024-12-03 12:51:44 -0300

    exercio 03

--- aula_02/exerciciosAula02.js
+++ aula_02/exerciciosAula02.js
@@ -8,7 +8,7 @@ let novoSalario = salario + abono
 console.log(`Seu novo salário é: ${new Intl.NumberFormat("pt-BR", {
     style: "currency",
     currency: "BRL"
-  }).format(novoSalario)}`)*/
+  }).format(novoSalario)}`)
 
   //Exercicio 02
 
@@ -19,6 +19,20 @@ console.log(`Seu novo salário é: ${new Intl.NumberFormat("pt-BR", {
   let nota4 = leia.questionFloat("Digite a nota 4: ");
   let media = (nota1 + nota2 + nota3 + nota4) / 4
   console.log(`Sua média é: ${media.toFixed(1)}`)
+
+*/
+
+//Exercio 3
+const leia = require("readline-sync");
+let salarioBruto = leia.questionFloat('Entre o valor do seu salário: ')
+let adicionalNoturno = leia.questionFloat("Entre o valor do seu adicional noturno: ");
+let horasExtras = leia.questionFloat("Entre o valor da sua hora extra: ");
+let desconto = leia.questionFloat("Entre o valor a ser descontado: ");
+let salarioLiquido = salarioBruto + adicionalNoturno + (horasExtras * 5) - desconto
+console.log(`Seu salário liquido é  ${new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL"
+  }).format(salarioLiquido)}`)
 
 
 
