f03584aa8749e78039226a2f99a6348f4d793006
gaba-teixeira <gaba.andrade@outlook.com>
2024-12-05 15:38:28 -0300

    aula 04

---
+++ aula_04/tabuada(DoWhile).js
@@ -0,0 +1,8 @@
+const leia = require("readline-sync");
+let numero = leia.questionInt("Digite um numero: ");
+let contador = 11;
+// Faz uma vez e depois testa a condicao
+do {
+  console.log(`${numero} x ${contador} = ${numero * contador}`);
+  contador++;
+} while (contador <= 10);
---
+++ aula_04/tabuada(while).js
@@ -0,0 +1,8 @@
+const leia = require("readline-sync");
+let numero = leia.questionInt("Digite um numero: ");
+let contador = 1;
+
+while (contador <= 10) {
+  console.log(`${numero} x ${contador} = ${numero * contador}`);
+  contador++;
+}
